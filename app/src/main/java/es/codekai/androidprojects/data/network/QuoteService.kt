a938d8d9faac0b2662f2d0118f1a26bdf906aebd
Juan R. Gavilanes <jrgavilanes@gmail.com>
2022-08-20 11:34:12 +0200

    small fixes

--- app/src/main/java/es/codekai/androidprojects/data/network/QuoteService.kt
+++ app/src/main/java/es/codekai/androidprojects/data/network/QuoteService.kt
@@ -1,13 +1,16 @@
 package es.codekai.androidprojects.data.network
 
 import es.codekai.androidprojects.data.model.QuoteModel
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class QuoteService @Inject constructor(private val api: QuoteApiClient) {
-    suspend fun getQuotes(): List<QuoteModel> {
-        return withContext(Dispatchers.IO) {
+class QuoteService @Inject constructor(
+    private val api: QuoteApiClient
+) {
+    suspend fun getQuotes(dispatcher: CoroutineDispatcher = Dispatchers.IO): List<QuoteModel> {
+        return withContext(dispatcher) {
             val response = api.getAllQuotes()
             response.body() ?: emptyList()
         }
